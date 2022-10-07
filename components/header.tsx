3fa07f878b5e1aedc908f2f52f99e331c02f42f0
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-10-07 15:08:21 -0700

    Added dukes logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next'
+import dukesLogo from '../assets/Duke-logo.svg'
+import Image from 'next/image'
 
 
 const Header: NextPage = () => {
   return (
     <header>
-        
-
+      <Image src={dukesLogo}
+             alt="Duke's Steakhouse beige logo"
+             width={200}
+      />
+      
     </header>
   )
 }
