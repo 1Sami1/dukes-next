a6703385bc5721d18835a4a2d7321a249ec27cc9
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-10-09 15:57:56 -0700

    Where im at

--- components/burger.tsx
+++ components/burger.tsx
@@ -2,17 +2,14 @@ import styles from '../styles/Home.module.scss'
 import React from 'react';
 import { bool, func } from 'prop-types';
 
-const Burger = ({ open, setOpen }) => {
+const Burger = () => {
     return(
-        <div className={styles.burgerIcon} open={open} onClick={() => setOpen(!open)} >
+        <div className={styles.burgerIcon}>
             <div></div>
             <div></div>
             <div></div>
         </div>
     )
 }
-Burger.propTypes = {
-    open: bool.isRequired,
-    setOpen: func.isRequired,
-  };
+
 export default Burger
--- components/header.js
+++ components/header.js
@@ -3,31 +3,45 @@ import dukesLogo from '../assets/Duke-logo.svg'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Burger from './burger'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
 const Header = () => {
-  const [open, setOpen] = useState(false)
+  const [navOpen, setNavOpen] = useState(false);
+  
+  const showHideNav = () => {
+    setNavOpen(!navOpen);
+}
 
+const isDesktop = (e) => {
+    if(e.matches)
+        setNavOpen(false);
+    }
+    useEffect(() => {
+      let mediaQuery = window.matchMedia('(min-width: 600px)');
+      mediaQuery.addEventListener('change', isDesktop);
+      // this is the cleanup function to remove the listener
+      return () => mediaQuery.removeEventListener('change', isDesktop);
+}, []);
+}
 
   return (
     <header className={styles.siteHeader}>
       <Image src={dukesLogo}
              alt="Duke's Steakhouse beige logo"
-             width={200}
+             width={150}
       />
   <div>
-      <ul className={styles.navUl} open={open} setOpen{setOpen} >
+      <ul className={styles.navUl} >
         <li className={styles.navLi}><Link href='/'>Home</Link></li>
         <li className={styles.navLi}><Link href='/menuPage'>Menu</Link></li>
         <li className={styles.navLi}><Link href='/aboutPage'>About</Link></li>
         <li className={styles.navLi}><Link href='/contactPage'>Contact</Link></li>
       </ul>
-      <Burger open={open} setOpen{setOpen} />
+      <Burger />
   </div>
     </header>
   )
-}
 
 export default Header
